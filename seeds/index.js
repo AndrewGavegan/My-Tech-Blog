a46360bb9915960ee236a02be9bbade4b14f71f2
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-27 14:36:43 +1000

    populating models and seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,23 @@
 const seedComments = require('./comment');
 const seedPosts = require('./post');
 const seedUsers = require('./user');
-
 const sequelize = require('../config/connection');
+
+const seed = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n--- DB SYCNED ---\n');
+
+    await seedComments();
+    console.log('\n--- COMMENT SYCNED ---\n');
+
+    await seedPosts();
+    console.log('\n--- POST SYCNED ---\n');
+
+
+    await seedUsers();
+    console.log('\n--- USER SYCNED ---\n');
+
+    process.exit(0);
+};
+
+seed();
