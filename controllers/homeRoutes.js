daf929c40e5f5b3126fb8c8f595251e15ccfb90c
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-12 09:41:51 +1000

    refactoring onepost route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,21 +51,24 @@ router.get('/', async (req, res) => {
 // getting one post from post id //
 router.get('/post/:id', async (req, res) => {
   try {
-    const data = await Post.findOne({
+    const postData = await Post.findOne({
       where: { id: req.params.id },
-      attributes: ['id', 'name', 'body'],
       include: [{
-        model: Comment, attributes: ['id', 'body', 'post_id', 'user_id'],
-        include: { model: User, attributes: ['username'] }
-      },
-      { model: User, attributes: ['username'] }]
+        model: Comment
+      }],
     });
-    if (!data) {
+    if (!postData) {
       res.status(404).json({ message: 'No post has this id' });
       return;
     }
-    const post = data.get({ plain: true });
-    res.render('onePost', { post, loggedIn: req.session.loggedIn });
+    const posts = postData.map(post => post.get({ plain: true }));
+
+    res.render('onepost', {
+      posts,
+      userId: req.session.user_id,
+      userName: req.session.name,
+      loggedIn: req.session.loggedIn
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
