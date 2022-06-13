86ee687a4415dedf7cc65776cbc404305f4bfaa5
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-13 14:55:32 +1000

    appending dash page

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -41,3 +41,5 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes')
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
