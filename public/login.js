4e70530bd9afadd18c20937e6eb48340cc95c471
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-13 12:08:56 +1000

    adding login and signup events

---
+++ public/login.js
@@ -0,0 +1,27 @@
+const loginSubmit = async (e) => {
+    e.preventDefault();
+
+    // fill in the element on the page to be selected //
+    const email = document.querySelector('#email-login').value.trim();
+    // fill in the element on the page to be selected //
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // api/users/login presuming we use that route //
+        const res = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (res.ok) {
+            // redirects to homepage if you login succesfully //
+            document.location.assign('/');
+        } else {
+            alert('Error, Incorrect username or password');
+        }
+    }
+};
+
+// presuming we name the form element an ID of loginForm //
+document.querySelector('.login-form').addEventListener('submit', loginSubmit);
---
+++ public/signup.js
@@ -0,0 +1,26 @@
+const signUpSubmit = async (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        // create a post request with the details entered
+        const res = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+            // redirect to homepage
+            document.location.replace('/');
+        } else {
+            //  alert user if request has failed
+            alert('Error creating user, password must be minimum 8 characters long');
+        }
+    }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signUpSubmit);
