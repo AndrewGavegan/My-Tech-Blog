131c232893bde21828259d88faa1d8323edd2cd5
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-13 12:40:07 +1000

    changes

--- public/signup.js
+++ public/signup.js
@@ -1,15 +1,15 @@
 const signUpSubmit = async (e) => {
     e.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && email && password) {
+    if (username && email && password) {
         // create a post request with the details entered
         const res = await fetch('/api/users/', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
