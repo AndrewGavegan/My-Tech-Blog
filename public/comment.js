97eda884e8935f60f146500cbff24729609ab88e
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-14 18:18:50 +1000

    fixing errors

--- public/comment.js
+++ public/comment.js
@@ -1,17 +1,18 @@
 async function addCommentHandle(e) {
     e.preventDefault();
 
-    const commentContent = document.querySelector('#commentText').value.trim();
+    //must use same name that you said you would search for in the router, eg body and body, or content and content, I forget this everytime!!!!
+    const body = document.querySelector('#commentText').value.trim();
     // copy from update.js, method for grabbing post id from the url //
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1];
 
-    if (commentContent) {
+    if (body) {
         const res = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
                 post_id,
-                commentContent
+                body
             }),
             headers: { 'Content-Type': 'application/json' }
         });
